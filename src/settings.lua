f99c096fa767ba8b3309ed6539dae8ecc1eb2751
LovelySanta <laurens.scheldeman@gmail.com>
2018-06-03 12:03:36 +0200

    Moved to seperate repository

---
+++ src/settings.lua
@@ -0,0 +1,8 @@
+
+Settings = {}
+
+Settings.storageItemName = "deepStorageChest"
+Settings.storageChestName = Settings.storageItemName .. "-inventory"
+Settings.storageMonitorName = Settings.storageItemName .. "-monitor"
+
+Settings.storageGuiName = "deepStorageGui"
